10104be27aa180e8967fb3d01fae5bb4fe48a33b
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-28 08:48:47 -0700

    Agregar archivos de proyecto.

---
+++ Baseballl/equipo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baseballl
+{
+    class equipo
+    {
+        public string Nombre {get; set;}
+        public string CiudadDeOrigen { get; set; }
+        public string Entrenador { get; set; }
+
+        public List<jugador> Jugadores { get; set; }
+
+        public equipo()
+        {
+            Jugadores = new List<jugador>();
+
+            Nombre = "Equipo Numero";
+            CiudadDeOrigen = "Ciudad Nueva";
+            Entrenador = "Entrenador Nuevo";
+        }
+
+        public equipo(string nombre)
+        {
+            Jugadores = new List<jugador>();
+
+            Nombre = Nombre;
+            CiudadDeOrigen = "Ciudad Nueva";
+            Entrenador = "Entrenador Nuevo";
+
+        }
+    }
+}
+ 
---
+++ Baseballl/jugador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baseballl
+{
+    class jugador
+    {
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+
+        public jugador ()
+        {
+            Nombre = "Midoriya";
+            Numero = "420";
+        }
+
+        public jugador(string nombre)
+        {
+            Nombre = nombre;
+            Numero = "420";
+        }
+    }
+}
